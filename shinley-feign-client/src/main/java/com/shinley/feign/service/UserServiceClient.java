70cf07d65cdb0bdf8994a66783433b82f1b98978
陈新磊 <chenxinlei666@163.com>
2018-08-26 00:07:12 +0800

    完善spring cloud config配置

--- shinley-feign-client/src/main/java/com/shinley/feign/service/UserServiceClient.java
+++ shinley-feign-client/src/main/java/com/shinley/feign/service/UserServiceClient.java
@@ -5,8 +5,8 @@ import com.shinley.cloud.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@FeignClient(value = "SHINLEY-FEIGN-PROVIDER", url = "http://localhost:3366")         // 直連
-@FeignClient(value = "SHINLEY-FEIGN-PROVIDER")      // 从eureka获取
+@FeignClient(value = "SHINLEY-FEIGN-PROVIDER", url = "${feign.provider.url}")         // 直連
+//@FeignClient(value = "SHINLEY-FEIGN-PROVIDER")      // 从eureka获取
 public interface UserServiceClient {
     @RequestMapping("/getUser")
     User getUser();
